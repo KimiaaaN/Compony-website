277e8d1510d48a98042ae647c78bd363f77f4a54
KimiaaaN <k.nahravanian@gmail.com>
2022-08-19 13:17:48 +0300

    add pages and update

---
+++ main.js
@@ -0,0 +1,33 @@
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 150) {
+      document.querySelector('#navbar').style.opacity = 0.9;
+    } else {
+      document.querySelector('#navbar').style.opacity = 1;
+    }
+  });
+  
+  
+  // Smooth Scrolling
+  $('#navbar a, .btn').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100
+        },
+        800
+      );
+    }
+  });
+
+  function sConsole(event1) {
+    event1.preventDefault();
+    var data = document.getElementById("name" );
+    console.log(data.value);
+    
+  };
